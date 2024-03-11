c4fab73157839ddf5f3c3d7b80eb0bb314ef152f
Abhinav Dubey <118627945+itsabhi-nav@users.noreply.github.com>
2024-03-11 08:18:16 +0530

    Add files via upload

---
+++ exp10/Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../C6416dsk.cmd 
+
+ASM_SRCS += \
+../Vectors_intr.asm 
+
+C_SRCS += \
+../c6416dskinit.c \
+../main.c 
+
+OBJS += \
+./Vectors_intr.obj \
+./c6416dskinit.obj \
+./main.obj 
+
+ASM_DEPS += \
+./Vectors_intr.pp 
+
+C_DEPS += \
+./c6416dskinit.pp \
+./main.pp 
+
+C_DEPS__QUOTED += \
+"c6416dskinit.pp" \
+"main.pp" 
+
+OBJS__QUOTED += \
+"Vectors_intr.obj" \
+"c6416dskinit.obj" \
+"main.obj" 
+
+ASM_DEPS__QUOTED += \
+"Vectors_intr.pp" 
+
+ASM_SRCS__QUOTED += \
+"../Vectors_intr.asm" 
+
+C_SRCS__QUOTED += \
+"../c6416dskinit.c" \
+"../main.c" 
+
+
